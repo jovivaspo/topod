6157a8f54cea508e029810c42b006c70d1d42d0a
Vivas Pocostales Jorge <jovivaspo@gmail.com>
2022-02-08 16:54:36 +0100

    responsive

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,45 +3,63 @@ import { makeStyles } from '@material-ui/styles';
 import React, { useState } from 'react';
 import FormLogin from '../components/FormLogin';
 
-const useStyles = makeStyles((theme)=>({
-    container:{
-        display:'flex',
-        justifyContent:'center',
-        width:'90vw',
+const useStyles = makeStyles((theme) => ({
+    container: {
+        display: 'flex',
+        justifyContent: 'center',
+        width: '90vw',
         margin: '0 auto',
-        marginTop:'80px',
-        gap:'50px'
+        marginTop: '80px',
+        gap: '50px',
+        [theme.breakpoints.down('sm')]: {
+            flexDirection: 'column',
+            marginTop: '20px',
+            marginBottom: '20px',
+        },
+        [theme.breakpoints.down('xs')]: {
+            gap: '30px',
+        },
+
+
     },
 
-    textBox:{
-        display:'flex',
-        flexDirection:'column',
-        gap:20,
-        marginTop:40
+    textBox: {
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 20,
+        marginTop: 40,
+        [theme.breakpoints.down('sm')]: {
+            alignItems: 'center',
+            marginTop: 40,
+        },
+        [theme.breakpoints.down('xs')]: {
+            alignItems: 'start',
+            marginTop: 10,
+        },
     },
 
-    title:{
-        textAlign:'center'
+    title: {
+        textAlign: 'center'
     }
 }))
 
 const Login = () => {
     const [value, setValue] = useState(0)
     const classes = useStyles()
-  return (<div className={classes.container}>
-      <div className={classes.textBox}>
-          <h2 className={classes.title}>ToPod</h2>
-          <p className={classes.text}>Escucha a tus youtubers favoritos desde donde quieras</p>
-          <p className={classes.text}>Crea tu playlist y descarga tus podcasts desde la app</p>
-          <p className={classes.text}>Convierte cualquier video a formtato mp3</p>
-      </div>
-      <Divider orientation="vertical" flexItem/>
-      <div>
-          {value===0? <h2>Inicia sesión</h2> : <h2>Crea una cuenta</h2>}
-          <FormLogin value={value} setValue={setValue} />
-      </div>
-      
-  </div>);
+    return (<div className={classes.container}>
+        <div className={classes.textBox}>
+            <h2 className={classes.title}>ToPod</h2>
+            <p className={classes.text}>Escucha a tus youtubers favoritos desde donde quieras</p>
+            <p className={classes.text}>Crea tu playlist y descarga tus podcasts desde la app</p>
+            <p className={classes.text}>Convierte cualquier video a formtato mp3</p>
+        </div>
+        <Divider orientation="vertical" flexItem />
+        <div>
+            {value === 0 ? <h2>Inicia sesión</h2> : <h2>Crea una cuenta</h2>}
+            <FormLogin value={value} setValue={setValue} />
+        </div>
+
+    </div>);
 };
 
 export default Login;

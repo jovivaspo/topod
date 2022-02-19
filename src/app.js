2fd615d68c719b1e3ce2967c840d2836c507970e
Vivas Pocostales Jorge <jovivaspo@gmail.com>
2022-02-19 12:47:47 +0100

    deployin

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const notFound = require('./middleware/notFound')
 const handleError = require('./middleware/handleError')
-
+const path = require('path')
 
 
 const app = express()
@@ -21,6 +21,22 @@ app.use('/api/user', require('../src/routes/user'))
 app.use('/api/videos', require('../src/routes/videos'))
 app.use('/api/podcasts', require('../src/routes/podcast'))
 app.use('/donaciones', require('../src/routes/donaciones'))
+
+//Despliegue
+
+__dirname = path.resolve()
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'/client/build')))
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}else{
+    app.get('/',(req,res)=>{res.send('Welcome')})
+}
+
+
+
+
 //Errors
 app.use(notFound)
 app.use(handleError)
